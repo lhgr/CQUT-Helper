403ae06d64fb5bacca13cfcc68b95ce5ca9b7ced
Dawn <3414950665@qq.com>
2026-04-16 00:07:22 +0800

    fix: 锁定应用为竖屏方向

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:cqut_helper/routes/Routes.dart';
 import 'package:cqut_helper/app/app_bootstrap.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await bootstrapAndRunApp(getRootWidget);
 }
